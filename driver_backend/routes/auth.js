4de9e11f94d98a642eb9cde3f4a726483cf2393f
Kim-jaeyeon <jy020507@naver.com>
2026-01-15 13:34:59 +0900

    chore: 백엔드 코드 삭제 (localStorage 기반으로 전환 완료)
    - driver_backend 폴더 전체 삭제
    - 백업은 backup/backend-with-api 브랜치에 보관됨

--- driver_backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login, logout } = require('../controllers/authController');
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', logout);
-
-module.exports = router;
